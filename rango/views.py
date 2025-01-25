18f214b4900c6f7bd5840c447950d76eb889c3ed
Aaron Hughes <2881785H@student.gla.ac.uk>
2025-01-25 00:20:56 +0000

    Chapter 13 (with extensions)

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,7 @@ from rango.forms import CategoryForm, PageForm # UserForm, UserProfileForm
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
+from rango.bing_search import run_query
 
 def index(request):
 
@@ -236,3 +237,25 @@ def visitor_cookie_handler(request):
         request.session['last_visit'] = last_visit_cookie
     
     request.session['visits'] = visits
+
+def search(request):
+
+    result_list = []
+
+    context_dict = {}
+
+    query = ''
+
+    if request.method == 'POST':
+
+        query = request.POST['query'].strip()
+
+        context_dict['query'] = query
+
+        if query:
+
+            result_list = run_query(query)
+
+            context_dict['result_list'] = result_list
+
+    return render(request, 'rango/search.html', context=context_dict)
